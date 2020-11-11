b0e20944a483bc13c7cd6c1999b795d60afd03d8
Laura Pulecio <lpulecio@unal.edu.co>
2020-11-11 16:17:34 -0500

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -6,21 +6,36 @@ Created on Wed Nov 11 11:02:29 2020
 """
 
 from tkinter import filedialog
-from tkinter import *
+import tkinter as tk
+import tkinter.font as tkFont
+from PIL import ImageTk, Image
+
+
+raiz = tk.Tk()
+raiz.geometry("1000x1000")
+raiz.title("Encriptación de imágenes")
+raiz.config(bg="white")
 
 def subir(event=None):
     filename = filedialog.askopenfilename()
     print('Selected:', filename)
+    aviso=tk.Label(raiz,text="A continuación podrá visualizar la imagen seleccionada", bg="white")
+    aviso.pack()
+    im=Image.open(filename)
+    img = ImageTk.PhotoImage(im)
+    img2=tk.Label(raiz, image=img)
+    img2.pack()
 
-raiz = Tk()
-raiz.title("Encriptación de imágenes")
-raiz.config(bg="blue")
+fontStyle= tkFont.Font(family="Lucida Grande", size=20)
+Titulo=tk.Label(raiz,text="Bienvenido a la plataforma de encriptación", bg="white", font=fontStyle)
+Titulo.pack()
 
 
-label = Label(raiz, text = "Selecciona la imagen a encriptar")
-button = Button(raiz, text = "Buscar",command=subir)
+label = tk.Label(raiz, text = "Selecciona la imagen a encriptar", bg="white")
+button = tk.Button(raiz, text = "Buscar",command=subir, activebackground="#5FB4EF", bg="white", overrelief="raised")
 label.pack()
 button.pack()
+
 
 raiz.mainloop()
 #para ver la ventana bien se puede agrandar con el mouse
