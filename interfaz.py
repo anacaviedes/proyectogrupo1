030c4138a3d64e4b42cec4a34e6bcb4fc541766e
anacaviedes <avcaviedesa@unal.edu.co>
2020-11-11 11:37:10 -0500

    Interfaz

---
+++ interfaz.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 11:02:29 2020
+
+@author: Usuario
+"""
+
+from tkinter import filedialog
+from tkinter import *
+
+def subir(event=None):
+    filename = filedialog.askopenfilename()
+    print('Selected:', filename)
+
+raiz = Tk()
+raiz.title("Encriptación de imágenes")
+raiz.config(bg="blue")
+
+
+label = Label(raiz, text = "Selecciona la imagen a encriptar")
+button = Button(raiz, text = "Buscar",command=subir)
+label.pack()
+button.pack()
+
+raiz.mainloop()
+#para ver la ventana bien se puede agrandar con el mouse
