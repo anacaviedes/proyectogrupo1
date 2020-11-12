68045caeff141e025fdd08853fb2b38e1a702e9d
Laura Pulecio <lpulecio@unal.edu.co>
2020-11-12 08:00:06 -0500

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -16,8 +16,8 @@ raiz.title("Encriptación de imágenes")
 raiz.config(bg="white")
 
 def descargar(event=None):
-    aviso4=Label(raiz,text="Su imagen se descargará")
-    aviso4.pack()
+    #aviso4=Label(raiz,text="Su imagen se descargará")
+    #aviso4.pack()
 
 def encriptar():
     aviso5=Label(raiz,text="Su imagen se va a encriptar")
@@ -34,22 +34,22 @@ def subir(event=None):
     aviso1=Label(raiz,text="A continuación podrá ver la imagen seleccionada",bg="white")
     aviso1.pack()
     img=ImageTk.PhotoImage(file=filename2)
-    img2=Label(raiz, image=img)
+    img2=Label(raiz, image=img, bg="white")
     img2.pack()
     button2= Button(raiz, text="Encriptar", command=encriptar, activebackground="#5FB4EF", bg="white", overrelief="raised")
     button2.pack()
+    #button3= Button(raiz, text="Descargar", command=descargar, activebackground="#5FB4EF", bg="white", overrelief="raised")
+    #button3.pack()
+
 
 scrollbar=Scrollbar(raiz)
-scrollbar.pack(side="right",fill="y")
-listbox=Listbox(raiz, yscrollcommand=scrollbar.set)
-scrollbar.config(command=listbox.yview)
 
 #imagenpru=ImageTk.PhotoImage(file="/Users/lithium/Documents/GitHub/proyectogrupo1/prueba.jpg")
 #imagenprueba=Label(raiz,image=imagenpru)
 #imagenprueba.pack()
 
 
-fontStyle= tkFont.Font(family="Lucida Grande", size=20)
+fontStyle= tkFont.Font(family="Lucida Grande", size=30)
 Titulo=Label(raiz,text="Bienvenido a la plataforma de encriptación", bg="white", font=fontStyle)
 Titulo.pack()
 
