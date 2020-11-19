d85199ba2fa0dc7c40c055201be50bfa2b21e094
anacaviedes <avcaviedesa@unal.edu.co>
2020-11-18 20:51:14 -0500

    Update pyaes_encriptado.py

--- pyaes_encriptado.py
+++ pyaes_encriptado.py
@@ -8,45 +8,57 @@ Created on Sun Nov 15 13:01:48 2020
 import pyaes
 import base64
 
-def encriptacion(plaintext, clave):
-   aes = pyaes.AESModeOfOperationCTR(clave)
-   return (base64.b64encode(aes.encrypt(plaintext))).decode("utf-8")
-
-def desencriptacion(encriptado, clave):
-    aes = pyaes.AESModeOfOperationCTR(clave)
-    return (aes.decrypt(base64.b64decode(encriptado))).decode("utf-8")
-
-
-a=int(input("Escriba 1 para encriptar o 2 para desencriptar: "))
-archivo=input("Escriba el nombre del archivo: ") #el nombre, no la ubicación; inclyendo el formato: .jpg
-clave=bytes(input("Ingrese la clave: "), 'utf-8') #16, 24 o 32 caracteres
-
-if a==1:   
-    with open(archivo, "rb") as img_file:
-        imagen64b1 = base64.b64encode(img_file.read())    
+class Encriptado:
     
-    encriptado = encriptacion(imagen64b1,clave)    
-    encriptada_copia=encriptado    
-
-    image = open(""+archivo, "wb")
-    image.write(base64.b64decode(encriptada_copia))
-    image.close()
-    print("Guardado ",""+archivo) #queda en el mismo directorio de este archivo de python
-
-else:       
-    with open(archivo, "rb") as img_file:
-        imagen64b2 = base64.b64encode(img_file.read())        
+    def __init__(self, nombre_archivo, accion):
+        self.archivo=nombre_archivo           
+        self.accion=accion              
+            
+    def encriptacion(self, clave):
+        if self.accion==1:
+            with open(self.archivo, "rb") as img_file:
+                self.imagen64b1 = base64.b64encode(img_file.read()) 
+            aes = pyaes.AESModeOfOperationCTR(clave)
+            self.encriptado=(base64.b64encode(aes.encrypt(self.imagen64b1))).decode("utf-8")
+            return self.encriptado
     
-    desencriptado=desencriptacion(imagen64b2,clave)
-    desencriptado_copia=desencriptado
+    def desencriptacion(self, clave):
+        if self.accion==2:
+            with open(archivo, "rb") as img_file:
+                self.imagen64b2 = base64.b64encode(img_file.read())      
+            aes = pyaes.AESModeOfOperationCTR(clave)
+            self.desencriptado=(aes.decrypt(base64.b64decode(self.imagen64b2))).decode("utf-8")
+            return self.desencriptado
     
-    image = open(""+archivo, "wb")
-    image.write(base64.b64decode(desencriptado_copia))
-    image.close()
-    print("Guardado", ""+archivo)
+    def guardar(self):
+        if self.accion==1:
+            image = open(""+self.archivo, "wb") #cambio de +archivo a +self.archivo hecho por Joel
+            image.write(base64.b64decode(self.encriptado))
+            image.close()
+            print("Guardado ",""+self.archivo) 
+            return self.archivo  #linea agregada por Joel          
+        if self.accion==2:
+            image = open(""+self.archivo, "wb")
+            image.write(base64.b64decode(self.desencriptado))
+            image.close()
+            print("Guardado ",""+self.archivo) 
 
-
-"""
-#clave128 = 16caracteres.LET
-#clave192 = 24caracteres.LET;-S_Nume
-#clave256 = 32caracteres.LETR;-SNume::-><+¡?"""
+if __name__=="__main__": #linea agregada por Joel 
+    a=int(input("Escriba 1 para encriptar o 2 para desencriptar: "))
+    archivo=input("Escriba el nombre del archivo: ") #el nombre, no la ubicación; inclyendo el formato: .jpg
+    clave=bytes(input("Ingrese la clave: "), 'utf-8') #16, 24 o 32 caracteres
+    
+    """
+    b=Encriptado(archivo, a)
+    b.encriptacion(clave)
+    b.guardar()"""
+    
+    """
+    c=Encriptado(archivo,a)
+    c.desencriptacion(clave)
+    c.guardar()"""
+    
+    """
+    #clave128 = 16caracteres.LET
+    #clave192 = 24caracteres.LET;-S_Nume
+    #clave256 = 32caracteres.LETR;-SNume::-><+¡?"""
