170f41d894022eff615b117c98a08b02e23612c0
ErickLeonel <73723739+ErickLeonel@users.noreply.github.com>
2020-11-18 20:42:11 -0500

    Update funcion_encriptado_y_desencriptado_rsa.py
    Correccion

--- funcion_encriptado_y_desencriptado_rsa.py
+++ funcion_encriptado_y_desencriptado_rsa.py
@@ -4,33 +4,33 @@ Created on Wed Nov 11 17:08:53 2020
 
 @author: ERICK
 """
-%pylab inline
+
 import matplotlib.pyplot as plt
 import numpy as np
 import encriptado_y_desencriptado_rsa
 
 def encriptado_imagen(direccion,n,e):
-    im=imread(direccion)
+    im=plt.imread(direccion)
     im=im.copy()
-    inf_extra = np.ones((shape(im)[0], shape(im)[1], shape(im)[2],3),dtype=int)
-    for i in range(shape(im)[0]):
-        for j in range(shape(im)[1]):
-            for k in range(shape(im)[2]):
+    inf_extra = np.zeros((np.shape(im)[0], np.shape(im)[1], np.shape(im)[2],3),dtype=int)
+    for i in range(np.shape(im)[0]):
+        for j in range(np.shape(im)[1]):
+            for k in range(np.shape(im)[2]):
+                if im[i,j,k]== 255:
+                    im[i,j,k]=254
                 a = encriptado_y_desencriptado_rsa.encripta_rsa(im[i,j,k],n,e)
-                if a[0] < 255:
-                    im[i,j,k] = a[0]
-                elif a[0] > 255:
-                    im[i,j,k] = a[2]
-                inf_extra[i, j, k, 0], inf_extra[i, j, k, 1], inf_extra[i, j, k, 2]= a[0], a[1], a[2]
+                im[i,j,k] = a[2]
+                inf_extra[i, j, k, 0], inf_extra[i, j, k, 1], inf_extra[i, j, k, 2] = a[0], a[1], a[2]
     return(im, inf_extra)
 
 
 def desencriptado_imagen(im,inf_extra,n,d):
     im=im.copy()
-    for i in range(shape(im)[0]):
-        for j in range(shape(im)[1]):
-            for k in range(shape(im)[2]):
-                p = encriptado_y_desencriptado_rsa.desencripta_rsa(inf_extra[i, j, k, 0], inf_extra[i, j, k, 1],inf_extra[i, j, k, 2], n, d)
-                im[i, j, k] = p
+    for i in range(np.shape(im)[0]):
+        for j in range(np.shape(im)[1]):
+            for k in range(np.shape(im)[2]):
+                im[i,j,k]=encriptado_y_desencriptado_rsa.desencripta_rsa(int(inf_extra[i, j, k][0]),int(inf_extra[i, j, k][1]),int(inf_extra[i, j, k][2]),n,d)
     return(im)
-imshow(encriptado_imagen('',n,d)[0])
+a=encriptado_imagen("C:\\Users\\ERICK\\Downloads\\SimpleBMP.bmp",109999,5)
+plt.imshow(a[0])
+plt.imshow()
